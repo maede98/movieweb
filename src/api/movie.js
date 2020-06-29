9e2f026433eaa6833e265ff13fb1f2348c668a1a
maede <m.moradian98@yahoo.com>
2020-06-29 12:25:55 +0430

    seventh commit

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const API_KEY = '7a00dc759d7fa7b0dd5562f7431d6ced';
 
 export default axios.create({
-    
+    baseURL:'https://api.themoviedb.org/3/movie/550?api_key=7a00dc759d7fa7b0dd5562f7431d6ced',
+    params :{
+        API_KEY : '7a00dc759d7fa7b0dd5562f7431d6ced',
+    },
+});
 
-    baseURL:'https://api.themoviedb.org/3/movie/550?api_key=7a00dc759d7fa7b0dd5562f7431d6ced'
-
-})
