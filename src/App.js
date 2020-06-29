3d94768de325ad16509ee81760794fd32f4c3ee0
maede <m.moradian98@yahoo.com>
2020-06-29 13:29:16 +0430

    tenth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import movie from './api/movie';
-import { Searchbar , Moviedeatail , Movielist} from './components';
+import { Searchbar , Moviedeatail , Movielist , Movieitem} from './components';
 
 class App extends React.Component{
 
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export {default as  Searchbar} from './Searchbar';
 export {default as  Moviedeatail} from './Moviedeatail';
 
 export {default as  Movielist} from './Movielist';
+
+export {default as Movieitem} from './Movieitem';
